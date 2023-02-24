c450b6272f8fbdc05596d80a6702be9884edafc3
Yancap <yangabriel3001f@gmail.com>
2023-02-24 15:21:21 -0300

    principais funções concluidas

---
+++ server/src/controller/LoginController.js
@@ -0,0 +1,24 @@
+const { hash, compare } = require("bcryptjs");
+const sqliteConnection = require("../database/sqlite");
+const AppError = require("../utils/AppError");
+
+class LoginController{
+    async show(request, response){
+        const { name, id } = request.body;
+        response.json({ message: "Sucesso", name: name, id: id});
+    }
+    async update(request, response){
+        const { id, newPassword, oldPassword } = request.body
+        const database = await sqliteConnection()
+        const checkData = await database.get("SELECT * FROM users WHERE id = ?", [id])
+        const checkPassword = await compare(oldPassword, checkData.password)
+        if(!checkPassword){
+            throw new AppError("Digite a Senha Correta")
+        }
+        const hashPassword = await hash(newPassword, 8)
+        await database.run("UPDATE users SET password = ? WHERE id = ?", [hashPassword, id])
+        response.json({message: "Senha alterada com sucesso"})
+    }
+}
+
+module.exports = LoginController
---
+++ server/src/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+require("express-async-errors")
+const AppError = require('./utils/AppError')
+
+const migrationsRun = require('./database/sqlite/migrations');
+const app = express();
+const routes = require('./routes');
+const { response } = require('express');
+
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use((error, request, response, next) =>{
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message,
+            type: error.type
+        })
+        
+    }
+    console.error(error);
+    return response.status(500).json({
+        status: "error",
+        message: "Erro do Servidor"
+    })
+})
+
+migrationsRun();
+
+const PORT = 3001;
+app.listen(PORT, ()=> console.log(`Server Running Port: ${PORT}`));
+
+
